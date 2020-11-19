e9500276cfcf0af5f0fa7c4d792f9371adeb88dd
Paola De la Cruz <paola.mapwire@gmail.com>
2020-11-19 20:55:46 -0300

    Prueba portfolio VFinal-Paola De la Cruz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,43 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('[data-toggle="tooltip"]').tooltip();
 
     /*Smooth Scroll*/
-    $("a").click(function(event) {
+    $("a").click(function (event) {
         if (this.hash !== "") {
             event.preventDefault();
 
             var gato = this.hash
             $("html, body").animate({
                 scrollTop: $(gato).offset().top
-            }, 800, function() {
+            }, 800, function () {
                 window.location.hash = gato;
             });
         }
     });
 
     /*GotoURL*/
-    $("#buttonMeetandCoffee").click(function(){
+    $("#buttonMeetandCoffee").click(function () {
         window.location.href = "https://github.com/paolafdlc/meetandcoffee";
     });
 
-    $("#buttonIguana").click(function(){
+    $("#buttonIguana").click(function () {
         window.location.href = "https://github.com/paolafdlc/Iguanapage";
     });
 
-    $("#buttonSuricata").click(function(){
+    $("#buttonSuricata").click(function () {
         window.location.href = "https://github.com/paolafdlc/desafiosuricata";
     });
 
-    $("#buttonCuppon").click(function(){
+    $("#buttonCuppon").click(function () {
         window.location.href = "https://github.com/paolafdlc/desafiocuppon";
     });
 
-    $("#buttonRicomida").click(function(){
+    $("#buttonRicomida").click(function () {
         window.location.href = "https://github.com/paolafdlc/desafioricomida";
     });
 
-    $("#buttonEspacioV").click(function(){
+    $("#buttonEspacioV").click(function () {
         window.location.href = "http://espaciov.cl/";
     });
-
-    
 });
