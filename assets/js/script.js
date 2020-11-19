58eb8b75775cc93942ac56f8d4bffa9d19b4cde9
Paola De la Cruz <paola.mapwire@gmail.com>
2020-11-19 20:48:52 -0300

    Prueba portfolio V1-Paola De la Cruz

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /*Smooth Scroll*/
+    $("a").click(function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var gato = this.hash
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800, function() {
+                window.location.hash = gato;
+            });
+        }
+    });
+
+    /*GotoURL*/
+    $("#buttonMeetandCoffee").click(function(){
+        window.location.href = "https://github.com/paolafdlc/meetandcoffee";
+    });
+
+    $("#buttonIguana").click(function(){
+        window.location.href = "https://github.com/paolafdlc/Iguanapage";
+    });
+
+    $("#buttonSuricata").click(function(){
+        window.location.href = "https://github.com/paolafdlc/desafiosuricata";
+    });
+
+    $("#buttonCuppon").click(function(){
+        window.location.href = "https://github.com/paolafdlc/desafiocuppon";
+    });
+
+    $("#buttonRicomida").click(function(){
+        window.location.href = "https://github.com/paolafdlc/desafioricomida";
+    });
+
+    $("#buttonEspacioV").click(function(){
+        window.location.href = "http://espaciov.cl/";
+    });
+
+    
+});
